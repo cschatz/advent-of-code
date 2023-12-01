24e8bd6d3f83ec187295783caaf2d2a6f3ff930f
Colin Schatz <schatz@amyris.com>
2023-11-30 22:38:58 -0800

    Cleanup

--- 2023/solvers/day1.py
+++ 2023/solvers/day1.py
@@ -2,6 +2,29 @@ import re
 
 from .base import Solver
 
+
+class Day1Solver(Solver):
+
+    def solve(self, part, *lines):
+        items = ["\\d"]
+        if part == 2:
+            items.extend(ENG_DIGITS.keys())
+        pattern = f"(?=({'|'.join(items)}))"
+
+        def interpret(line):
+            all = re.findall(pattern, line)
+            # overkill for just first and last, but easy to
+            # adapt to more than 2 digits
+            tot = 0
+            for d in (all[0], all[-1]):
+                tot *= 10
+                n = int(d) if d.isdigit() else ENG_DIGITS[d]
+                tot += n
+            return tot
+
+        return sum(interpret(line) for line in lines)
+
+
 ENG_DIGITS = dict(
     one=1,
     two=2,
@@ -13,33 +36,3 @@ ENG_DIGITS = dict(
     eight=8,
     nine=9,
 )
-
-PLAIN_PAT = "(?=(\\d))"
-EXPANDED_PAT = f"(?=(\\d|{'|'.join(ENG_DIGITS.keys())}))"
-
-
-def make_pattern(part):
-    items = ["\\d"]
-    if part == 2:
-        items.extend(ENG_DIGITS.keys())
-    return f"(?=({'|'.join(items)}))"
-
-
-def to_int(s):
-    if s.isdigit():
-        return int(s)
-    else:
-        return ENG_DIGITS[s]
-
-
-class Day1Solver(Solver):
-
-    def solve(self, part, *lines):
-        pat = make_pattern(part)
-
-        def interpret(line):
-            all = re.findall(pat, line)
-            digits = tuple(map(to_int, (all[0], all[-1])))
-            return int(digits[0]) * 10 + int(digits[1])
-        return sum(interpret(line) for line in lines)
-
