9e64fc203f1deae7b619a4ff9749012b2e38bca8
Colin Schatz <schatz@amyris.com>
2023-12-22 12:26:55 -0800

    Day 21 part 1

---
+++ advent/puzzles_2023/solvers/day21.py
@@ -0,0 +1,58 @@
+from .base import Solver
+
+
+CARDINAL = ((0, 1), (0, -1), (1, 0), (-1, 0))
+
+
+class Day21(Solver):
+    def parse(self, lines):
+        self.grid = lines
+        self.height = len(lines)
+        self.width = len(lines[0])
+        find_start = (
+            (r, c)
+            for r, row in enumerate(lines)
+            for c, x in enumerate(row)
+            if x == "S"
+        )
+        self.start = next(find_start)
+
+    def in_bounds(self, r, c):
+        return r >= 0 and r < self.height and c >= 0 and c < self.width
+    
+    def neighbors(self, r, c, omit):
+        candidates = ((r + dr, c + dc) for dr, dc in CARDINAL)
+        return (
+            (rr, cc) for rr, cc in candidates
+            if self.in_bounds(rr, cc)
+            and (rr, cc) not in omit
+            and self.grid[rr][cc] != "#"
+        )
+    
+
+class Day21Part1(Day21):
+    def plots_reachable(self, tot_steps):
+        visited = set()
+        reachable = set()
+        q = [(self.start, tot_steps)]
+        while q:
+            pos, steps_left = q.pop(0)
+            if pos in visited:
+                continue
+            visited.add(pos)
+            if steps_left % 2 == 0:
+                reachable.add(pos)
+            if steps_left > 0:
+                for p in self.neighbors(*pos, visited):
+                    q.append((p, steps_left - 1))
+        return len(reachable)
+
+    def solve(self):
+        if self.width < 20:  # test
+            return self.plots_reachable(6)
+        else:
+            return self.plots_reachable(64)
+
+class Day21Part2(Day21):
+    def solve(self):
+        ...
