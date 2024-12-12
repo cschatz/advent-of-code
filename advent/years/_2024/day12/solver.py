e5bd89fe0c9392daa5b3137b9695d6e7995d5ebb
Colin Schatz <schatz@amyris.com>
2024-12-11 23:49:58 -0800

    Day 12

---
+++ advent/years/_2024/day12/solver.py
@@ -0,0 +1,92 @@
+from collections import defaultdict
+# Note: needs python 3.10 or newer
+from itertools import pairwise
+
+from advent.solver_base import Solver
+
+
+DIRS = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+
+def vadd(a, b):
+    return tuple(ai + bi for ai, bi in zip(a, b))
+
+def make_fence(r, c, dr, dc):
+    if dr:
+        # horizontal fence: ( above/below, (row below fence), col )
+        return ("below" if dr > 0 else "above", (dr + 1) // 2 + r, c)
+    else:
+        # vertical fence: ( left/right, col to right of fence), row )
+        return ("right" if dc > 0 else "left", (dc + 1) // 2 + c, r)
+
+
+class Day12(Solver):
+    def parse(self, lines):
+        self.garden = lines
+        self.rows = len(lines)
+        self.cols = len(lines[0])
+
+    def get(self, r, c):
+        return self.garden[r][c]
+    
+    def in_bounds(self, r, c):
+        return all((r >= 0, c >= 0, r < self.rows, c < self.cols))
+
+    def survey_region(self, start_plot):
+        plant_type = self.get(*start_plot)
+        in_region = set()
+        fences = set()
+        stack = [start_plot]
+        while len(stack) > 0:
+            current = stack.pop()
+            if current not in in_region:
+                in_region.add(current)
+                for dir in DIRS:
+                    neighbor = vadd(current, dir)
+                    if not self.in_bounds(*neighbor) or self.get(*neighbor) != plant_type:
+                        fences.add(make_fence(*current,* dir))
+                    else:
+                        stack.append(neighbor)
+        return len(in_region), fences, in_region
+    
+    def get_regions(self):
+        unsurveyed_plots = set([
+            (r, c)
+            for r in range(self.rows)
+            for c in range(self.cols)
+        ])
+        regions = []
+        while unsurveyed_plots:
+            plot = next(iter(unsurveyed_plots))
+            area, fences, plots = self.survey_region(plot)
+            unsurveyed_plots -= plots
+            regions.append((area, fences))
+        return regions
+
+class Part1(Day12):
+    def solve(self):
+        regions = self.get_regions()
+        return sum(
+            area * len(fences) for area, fences in regions
+        )
+    
+
+def count_sides(fences):
+    groups = defaultdict(set)
+    for side, group_num, pos in fences:
+        groups[(side, group_num)].add(pos)
+    return sum(
+        sum(
+            b - a > 1
+            for a, b in pairwise(sorted(positions))
+        ) + 1
+        for positions in groups.values()
+    )
+        
+
+class Part2(Part1):
+    def solve(self):
+        regions = self.get_regions()
+        return sum(
+            area * count_sides(fences) for area, fences in regions
+        )
